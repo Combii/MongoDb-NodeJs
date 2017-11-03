599507ba410290d5c81852d1a9eccb79e9293140
Combii <davidstovle@gmail.com>
2017-11-03 17:56:30 +0100

    somewhat working node

--- routes/orders.js
+++ routes/orders.js
@@ -1,12 +1,14 @@
+const MongoClient = require('mongodb');
 const express = require('express');
+const ObjectId = require('mongodb').ObjectID;
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+//Needed for req.body
 app.use(bodyParser.json());
 
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
 
 const mongoDbUrl = "mongodb://combii:1234@cluster-shard-00-00-uxhgu.mongodb.net:27017,cluster-shard-00-01-uxhgu.mongodb.net:27017,cluster-shard-00-02-uxhgu.mongodb.net:27017/zalandodummy?ssl=true&replicaSet=Cluster-shard-0&authSource=admin";
 
@@ -19,7 +21,7 @@ const mongoDbUrl = "mongodb://combii:1234@cluster-shard-00-00-uxhgu.mongodb.net:
 app.get('/orders', function (req, res) {
 
     MongoClient.connect(mongoDbUrl).then((db) => {
-        var col = db.collection('customers');
+        var col = db.collection('orders');
 
         col.find().toArray().then((result) => {
             res.json(result)
@@ -36,7 +38,7 @@ app.get('/orders', function (req, res) {
 app.get('/orders/:id', function (req, res) {
 
     MongoClient.connect(mongoDbUrl).then((db) => {
-        var col = db.collection('customers');
+        var col = db.collection('orders');
 
         col.findOne({'_id': ObjectId(req.params.id)}).then((result) => {
             res.json(result);
@@ -66,7 +68,7 @@ app.post('/orders/', function (req, res) {
         });
 
 
-        /*ordersTotal.products = [];
+        ordersTotal.products = [];
 
         req.body.products.forEach(function (element, index, array) {
 
@@ -75,20 +77,18 @@ app.post('/orders/', function (req, res) {
 
 
                 if (index === array.length - 1) {
-                    orderCol.insertOne(ordersTotal, function (err, result) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            res.status(201);
-                            res.json({msg: 'Order Created'});
-                            console.log(ordersTotal);
-                        }
+                    orderCol.insertOne(ordersTotal).then(() => {
+                        res.status(201);
+                        res.json({msg: 'Order Created'});
+                        console.log(ordersTotal);
+                    }).catch((err) => {
+                        console.log("ERROR!: " + err);
                     });
                     db.close();
                 }
 
             });
-        });*/
+        });
 
 
     });
